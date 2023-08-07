eb251fefccbd0aeea95f498a1353da45896543e4
Manchany777 <114803559+Manchany777@users.noreply.github.com>
2023-08-08 03:52:04 +0900

    [Bronze IV] Title: 영수증, Time: 232 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();; // 총금액
+		int N = sc.nextInt();  // 구매한 물건의 종류의 수
+		int a = 0; // 긱 물건의 가격
+		int b = 0; // 각 물건의 개수
+		int total = 0; // 각 물건 가격 * 개수 * 종류의수
+		
+		int[][] arr = new int[N][2]; // 가격과 개수 입력배열
+		
+		for(int i=0; i < arr.length; i++) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			arr[i][0] = a;
+			arr[i][1] = b;
+			total += arr[i][1] * arr[i][0];
+		}//for
+		sc.close();
+
+		if (X == total) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}//if
+		
+	}
+}
