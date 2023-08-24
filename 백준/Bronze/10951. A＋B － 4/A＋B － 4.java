a1169124d8848bae844ed9661bc74be6f95422a7
Manchany777 <114803559+Manchany777@users.noreply.github.com>
2023-08-25 02:22:24 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String str;
+
+		while((str=br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a+b).append("\n");
+		}//while
+		br.close();
+		System.out.print(sb);
+	}
+}
